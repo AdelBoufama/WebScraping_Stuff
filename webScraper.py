5adea4ef5790b631c1c2c6b01cf839e178eef87d
Adel Boufama <35932140+AlexBoufama@users.noreply.github.com>
2018-05-11 15:24:26 -0400

    Add files via upload

--- webScraper.py
+++ webScraper.py
@@ -3,64 +3,110 @@ import csv
 import StringIO
 import requests
 import pdb
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 import itertools
 from itertools import izip_longest
 #from itertools import zip_longest
 
-#url = "http://www.cn411.ca/main02view04.aspx?LinkTreeID=S020131&PageSize=100&PageID=1"
-url = "http://www.cn411.ca/main02view04.aspx?LinkTreeID=S020505&PageSize=10&PageID=1"
-
-r = requests.get(url)
-soup = BeautifulSoup(r.content)
+#url = "http://www.cn411.ca/main02view04.aspx?LinkTreeID=S020907&PageSize=100&PageID=1&AreaID=416"
+#
+#r = requests.get(url)
+#soup = BeautifulSoup(r.content)
 
 #pages = soup.find("span",{"id":"ctl00_cphRight_Main0102view_1_lbPager"})
 #lastPage = str(pages)[-9:-7]
 
-g_data = soup.find("table",{"id":"ctl00_cphRight_Main0102view_1_DataList1"})
+#g_data = soup.find("table",{"id":"ctl00_cphRight_Main0102view_1_DataList1"})
 
-pageNum = 0
+#pageNum = 0
 
-while g_data is not None:
-    pageNum = pageNum + 1
-    url = url[:-1] + str(pageNum)
-    #url = "http://www.cn411.ca/main02view04.aspx?LinkTreeID=S020131&PageSize=100&PageID=" + str(pageNum)
+def scrapeMainPage(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content)
+
     g_data = soup.find("table",{"id":"ctl00_cphRight_Main0102view_1_DataList1"})
-    if g_data is None:
-        break
 
-    names = g_data.find_all("a",{"class":"tree12"})
-    contacts = g_data.find_all("span",{"class":"T12"})
-    numberList = []
-    nameList = []
-    ratingList = []
-    data = [nameList,numberList,ratingList]
+    pageNum = 0
+    while g_data is not None:
+        pageNum = pageNum + 1
+        url = url[:-12] + str(pageNum) + "&AreaID=416"
+        print url
+        #url = "http://www.cn411.ca/main02view04.aspx?LinkTreeID=S020131&PageSize=100&PageID=" + str(pageNum)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content)
+        g_data = soup.find("table",{"id":"ctl00_cphRight_Main0102view_1_DataList1"})
 
-    for name in names:
-        # pdb.set_trace()
-        name = name.text.encode('utf8')
-        nameList.append(name)
+        names = g_data.find_all("a",{"class":"tree12"})
+        contacts = g_data.find_all("span",{"class":"T12"})
+        hyperLinks = g_data.find_all("a")
 
-    for item in g_data:
-        try:
-            phoneNum = item.contents[1].find_all("span",{"class":"T12"})[1].text.encode('utf8')
-            numberList.append(phoneNum)
+        numberList = []
+        nameList = []
+        ratingList = []
+        addressList =[]
+        postalCodeList = []
 
-        except:
-            pass
+        data = [nameList,numberList,ratingList,addressList,postalCodeList]
 
-    for item in g_data:
-        try:
-            rating = item.contents[1].find_all("span",{"class":"T12"})[2].text.encode('utf8')
-            ratingList.append(rating[30:len(rating)])
-        except:
-            pass
+        for name in names:
+            name = name.text.encode('utf8')
+            nameList.append(name)
 
-    export_data = izip_longest(*data, fillvalue = '')
+        for link in hyperLinks:
+            locationData = scrapeInnerPage(url[0:20] + link["href"])
+            addressList.append(locationData[0])
+            postalCodeList.append(locationData[1])
+            #print url[0:20] + link["href"]
 
-    with open('data.csv','a') as file:
-        writer = csv.writer(file)
-        writer.writerow(("nameList","numberList","ratingList"))
-        writer.writerows(export_data)
+        for item in g_data:
+            try:
+                phoneNum = item.contents[1].find_all("span",{"class":"T12"})[1].text.encode('utf8')
+                numberList.append(phoneNum)
+
+            except:
+                pass
+
+        for item in g_data:
+            try:
+                rating = item.contents[1].find_all("span",{"class":"T12"})[2].text.encode('utf8')
+                ratingList.append(rating[30:len(rating)])
+            except:
+                pass
+
+        export_data = izip_longest(*data, fillvalue = '')
+
+        with open('data.csv','a') as file:
+            writer = csv.writer(file)
+            #writer.writerow(("nameList","numberList","ratingList"))
+            writer.writerows(export_data)
+
+
+def scrapeInnerPage(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content)
+    addressData = soup.find("span",{"id":"ctl00_cphLeft_Views1_txtLinkAddress"})
+    addressDataM = soup.find("span",{"id":"ctl00_cphLeft_Viewms1_txtLinkAddress"})
+    postalCodeData = soup.find("span",{"id":"ctl00_cphLeft_Views1_txtLinkPostalCode"})
+    postalCodeDataM = soup.find("span",{"id":"ctl00_cphLeft_Viewms1_txtLinkPostalCode"})
+
+    if addressData is not None:
+        address = addressData.text.encode('utf8')
+    elif addressDataM is not None:
+        address = addressDataM.text.encode('utf8')
+    else:
+        address = 'N/A'
+
+    if postalCodeData is not None:
+        postalCode = postalCodeData.text.encode('utf8')
+    elif postalCodeDataM is not None:
+        postalCode = postalCodeDataM.text.encode('utf8')
+    else:
+        postalCode = 'N/A'
+
+
+    print address
+    return [address, postalCode]
+
+
+scrapeMainPage\
+("http://www.cn411.ca/main02view04.aspx?LinkTreeID=S020101&PageSize=100&PageID=1&AreaID=416")
